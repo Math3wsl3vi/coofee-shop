96205bd7875aa407f22b4f0e54ffc207debb749e
math3wsl3vi <levimathews9841@gmail.com>
2025-02-21 22:03:11 +0300

    menu changes

--- components/home/Menu.tsx
+++ components/home/Menu.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='mt-20 w-full flex justify-center flex-col items-center'>
+        <h1 className='text-2xl uppercase text-brown-2 text-center'>OUR MENU</h1>
+        <h2 className='text-3xl uppercase text-brown-2 text-center'>{"Today's "}Specials</h2>
+        <p className='text-center text-gray-400 text-xl w-1/2 mt-10 '>Proin consectetur non dolor vitae pulvinar. Pellentesque sollicitudin dolor eget neque viverra, sed interdum metus interdum. Cras lobortis pulvinar dolor, sit amet ullamcorper dolor iaculis vel</p>
+    </div>
   )
 }
 
